cac6890192c4ba12ebd1be33d8bc79e36e8a9b68
Devin.Zhu <chooin@126.com>
2020-11-11 00:52:30 +0800

    feat: 支持 usePageInterval

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from 'react-native-lifecycle';
 export { default as usePermissions } from './hooks/use-permissions';
 export { default as usePageEventEmitter } from './hooks/use-page-event-emitter';
-export { default as usePageInterval } from './use-page-interval';
+export { default as usePageInterval } from './hooks/use-page-interval';
