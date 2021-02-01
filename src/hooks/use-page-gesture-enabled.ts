53a82135b5520b40ee5aea6f95e14ec105cc925e
Devin.Zhu <chooin@126.com>
2021-02-01 11:19:38 +0800

    feat: usePageGestureEnabled 支持  setEnabled

--- src/hooks/use-page-gesture-enabled.ts
+++ src/hooks/use-page-gesture-enabled.ts
@@ -1,26 +1,36 @@
+import { useRef } from 'react';
 import { useLoad, useUnload } from 'react-native-lifecycle';
 import { useNavigation } from '@react-navigation/native';
 import { BackHandler } from 'react-native';
 
 export default (enabled: boolean) => {
+  const enabledRef = useRef(enabled);
   const navigation = useNavigation();
 
-  const _hardwareBackPress = () => {
-    if (enabled) {
+  const _onHardwareBackPress = () => {
+    if (enabledRef.current) {
       return false;
     } else {
       return true;
     }
   };
 
+  const setEnabled = (enabled: boolean) => {
+    enabledRef.current = enabled;
+  };
+
   useLoad(() => {
     navigation.setOptions({
       gestureEnabled: enabled,
     });
-    BackHandler.addEventListener('hardwareBackPress', _hardwareBackPress);
+    BackHandler.addEventListener('hardwareBackPress', _onHardwareBackPress);
   });
 
   useUnload(() => {
-    BackHandler.removeEventListener('hardwareBackPress', _hardwareBackPress);
+    BackHandler.removeEventListener('hardwareBackPress', _onHardwareBackPress);
   });
+
+  return {
+    setEnabled,
+  };
 };
